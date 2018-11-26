99ca61d08a2371879532f8153c2ed2a9468ea630
Nicklas Boserup <nickeopti@gmail.com>
2018-11-26 15:30:27 +0100

    Add new handler modules to the list

--- data/handler.js
+++ data/handler.js
@@ -2,7 +2,9 @@ const homezone_wheel = require('./homezone-wheel')
 const homezone_points = require('./homezone-points')
 const zone_count = require('./zone-count')
 const zone_act = require('./zone-act')
-const geocode_zone = require('./geocode-zone')
+const geocode_zone = require('./geocode')
+const coordinates_zone = require('./coordinates-zone')
+const zone_perimeter = require('./zone-perimeter')
 
 /**
  * Dictionary of currently available data-handlers
@@ -12,7 +14,9 @@ const handlers = {
     'zone-count': zone_count,
     'homezone-wheel': homezone_wheel,
     'homezone-points': homezone_points,
-    'geocode-zone': geocode_zone
+    'geocode-zone': geocode_zone,
+    'coordinates-zone': coordinates_zone,
+    'zone-perimeter': zone_perimeter
 }
 
 module.exports = {
