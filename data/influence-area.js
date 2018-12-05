28eaa08acdf7b6e3dc8206f11c94fe3a9f363e34
Nicklas Boserup <nickeopti@gmail.com>
2018-12-05 12:23:22 +0100

    Implement weekly 'area of influence' total

--- data/influence-area.js
+++ data/influence-area.js
@@ -14,14 +14,24 @@ const queryString =
             ON zone.gid = hz.home_hz
         WHERE hz.zone_hz = $1 AND hz.days_hz = $2;`
 
+const altQueryString = 
+    `SELECT SUM(AREA) as AREA
+        FROM (
+            SELECT DISTINCT ON (home_hz)
+                    ST_Area(zone.geom::geography)/1000000 as AREA
+                FROM mtc_homezone AS hz
+                INNER JOIN mtc AS zone
+                    ON zone.gid = hz.home_hz
+                WHERE hz.zone_hz = $1) s;`;
+
 /**
 * Requires the _zone_ and _day_ as query arguments
 */
 module.exports = {
     getDataAsJSON: (query, callback) => {
         let sqlQuery = {
-            text: queryString,
-            values: [query.zone, query.day]
+            text: query.day === undefined ? altQueryString : queryString,
+            values: query.day === undefined ? [query.zone] : [query.zone, query.day]
         }
         db.query(sqlQuery, (err, res) => {
             callback(res.rows[0])
